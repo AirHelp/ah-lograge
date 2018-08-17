7866c425e326208cc0d3acf6023524c73e8ea4f2
Przemysław Wróblewski <przemyslaw.wroblewski@gmail.com>
2018-08-17 15:20:26 +0200

    use Ah::Lograge.additional_custom_entries to collect http times httparty ControllerRuntime only needed when lograge is disabled

--- lib/ah/lograge/httparty/controller_runtime.rb
+++ lib/ah/lograge/httparty/controller_runtime.rb
@@ -1,6 +1,11 @@
 module Ah
   module Lograge
     module HTTParty
+      #
+      # that concern is only used on local development with lograge disabled
+      # it will log total time spent in http calls to other services along views & db times:
+      # (Views: 71.7ms | ActiveRecord: 70.7ms | HttpAirspace: 9820.2ms | HttpHermanStaging: 380.2ms)
+      #
       module ControllerRuntime
         extend ActiveSupport::Concern
 
@@ -19,10 +24,7 @@ module Ah
             messages, http_runtime = super, payload[:ah_http_runtime]
             http_runtime.each do |service, time|
               if time > 0
-                messages << ("Http(#{ service }): %.1fms" % time)
-                if defined?(Statsd) && defined?($statsd)
-                  $statsd.timing("http/#{ service }", time)
-                end
+                messages << ("#{ service.titleize.tr(' ', '') }: %.1fms" % time)
               end
             end
             messages
--- lib/ah/lograge/httparty/railtie.rb
+++ lib/ah/lograge/httparty/railtie.rb
@@ -2,10 +2,27 @@ module Ah
   module Lograge
     module HTTParty
       class Railtie < ::Rails::Railtie
+        STRIP_SUFFIX = %r[.ahinternal.net]
+        PREFIX = 'http_'
+
         config.after_initialize do |app|
           Ah::Lograge::HTTParty::LogSubscriber.attach_to :http
-          ActiveSupport.on_load(:action_controller) do
-            include Ah::Lograge::HTTParty::ControllerRuntime
+          if !app.config.lograge.enabled && Rails.env.development?
+            ActiveSupport.on_load(:action_controller) do
+              include Ah::Lograge::HTTParty::ControllerRuntime
+            end
+          end
+          previous_additional_custom_entries_block = nil
+          if Ah::Lograge.additional_custom_entries_block.respond_to?(:call)
+            previous_additional_custom_entries_block = Ah::Lograge.additional_custom_entries_block
+          end
+          Ah::Lograge.additional_custom_entries do |event|
+            http_runtime = Ah::Lograge::HTTParty::LogSubscriber.reset_runtime
+            # there might be a custom block defined already
+            if previous_additional_custom_entries_block
+              http_runtime.merge!(previous_additional_custom_entries_block.call(event))
+            end
+            http_runtime
           end
         end
       end
