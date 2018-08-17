ef9d3bc0661b7a031940c242d88388ee144824e7
Przemysław Wróblewski <przemyslaw.wroblewski@gmail.com>
2018-08-17 15:21:17 +0200

    move statsd to log_subscriber

--- lib/ah/lograge/httparty/instrumentation.rb
+++ lib/ah/lograge/httparty/instrumentation.rb
@@ -7,9 +7,6 @@ module Ah
         class_methods do
           def perform_request(http_method, path, options, &block) #:nodoc:
             uri = URI.parse(base_uri)
-            if defined?(Statsd) && defined?($statsd)
-              $statsd.increment("http/#{ uri.host }")
-            end
             ActiveSupport::Notifications.instrument("request.http", hostname: uri.host) do
               super
             end
